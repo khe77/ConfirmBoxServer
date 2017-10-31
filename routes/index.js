a01b134c511674e41192fb075083c9820c90f9d7
khe77 <khe7758@gmail.com>
2017-10-31 13:46:27 +0900

    ConfirmBoxServer API v0.1

--- routes/index.js
+++ routes/index.js
@@ -35,31 +35,172 @@ router.post('/login', function(req, res) {
 					res.send(JSON.stringify({}));
 				}				
 			}
-		}
-	);
+		});
 });
 
-router.post('/confirm', function(req, res) {
-	var en = req.body.en;
+router.get('/user',function(req,res) {
+	connection.query('select * from users', 
+		function(err,results,fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(results));
+			}
+		});
+});
+
+router.get('/user/:en',function(req,res){
+	connection.query('select * from users where en=?',
+		[ req.params.en ], function(err, results, fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				if (results.length > 0) {
+					res.send(JSON.stringify(results[0]));
+				} else {
+					res.send(JSON.stringify({}));
+				}
+				
+			}
+		});
+});
+
+router.post('/user',function(req,res){
 	var pw = req.body.pw;
+	var hash = crypto.createHash('sha256').
+		update(pw).digest('base64');
 	connection.query(
-		'select en,name,pw from users where en=? and pw=?',
-		[ en,pw ],
+		'insert into users(en,name,pw) values(?,?,?)',
+		[ req.body.en, req.body.name, hash ], 
+		function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+router.put('/user/:en',function(req,res){
+	var pw = req.body.pw;
+	var hash = crypto.createHash('sha256').
+		update(pw).digest('base64');
+	connection.query(
+		'update users set name=?,pw=? where en=?',
+		[ req.body.name, hash, req.params.en ],
+		function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+router.delete('/user/:en',function(req,res){
+	connection.query('delete from users where en=?',
+		[ req.params.en ], function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+router.get('/confirm', function(req, res) {
+	connection.query(
+'select confirm.en           as en           ' +
+'    ,  confirm.task_id      as task_id      ' +
+'    ,  confirm.cfm_seq      as cfm_seq      ' +
+'    ,  confirm.cfm_en       as cfm_en       ' +
+'    ,  users.name           as cfm_name     ' +
+'    ,  confirm.cfm_title    as cfm_title    ' +
+'    ,  confirm.cfm_text     as cfm_text     ' +
+'from   confirm                              ' +
+'left join users                             ' +
+'on     confirm.cfm_en = users.en            ' +
+'where  confirm.en=?                         ' +
+'and    confirm.task_id=?                    ' +
+'and    confirm.cfm_seq=?                    ',
+		[ req.query.en, req.query.task_id, req.query.cfm_seq ],
 		function(err,results,fields) {
 			if (err) {
 				res.send(JSON.stringify(err));
 			} else {
 				if (results.length > 0) {
-					res.send(JSON.stringify(results[0]));	
+					res.send(JSON.stringify(results));	
 				} else {
 					res.send(JSON.stringify({}));
 				}				
 			}
-		}
-	);
+		});
 });
 
-router.get('/list', function(req, res) {
+router.post('/confirm', function(req, res) {
+	var cfm_seq = '';
+	connection.query(
+		'select ifnull(max(cfm_seq)+1, 1) as cfm_seq from confirm where en=? and task_id=?',
+		[ req.body.en, req.body.task_id ],
+		function(err,results,fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				cfm_seq = results[0].cfm_seq;			
+			}
+		});
+	connection.query(
+		'insert into confirm(en,task_id,cfm_seq,cfm_en,cfm_title,cfm_text) values(?,?,?,?,?,?)',
+		[ req.body.en, req.body.task_id, cfm_seq, req.body.cfm_en, req.body.cfm_title, req.body.cfm_text ],
+		function(err,result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+router.delete('/confirm', function(req, res) {
+	connection.query(
+		'delete from confirm where en=? and task_id=? and cfm_seq=? ',
+		[ req.body.en, req.body.task_id, req.body.cfm_seq ],
+		function(err,result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+router.put('/confirm', function(req, res) {
+	connection.query(
+		'update confirm set cfm_yn="Y",cfm_opinion=? where en=? and task_id=? and cfm_seq=?',
+		[ req.body.en, req.body.task_id, req.body.cfm_seq, req.body.cfm_opinion ],
+		function(err,result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+router.put('/reject', function(req, res) {
+	connection.query(
+		'update confirm set cfm_yn="N",cfm_opinion=? where en=? and task_id=? and cfm_seq=?',
+		[ req.body.en, req.body.task_id, req.body.cfm_seq, req.body.cfm_opinion ],
+		function(err,result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
+});
+
+router.get('/confirms', function(req, res) {
 	var en = req.query.en;
 	connection.query(
 'select confirm.en           as en           ' +
@@ -85,96 +226,48 @@ router.get('/list', function(req, res) {
 					res.send(JSON.stringify({}));
 				}				
 			}
-		}
-	);
-});
-
-router.put('/confirm', function(req, res) {
-	var en = req.body.en;
-	var task_id = req.body.task_id;
-	var cfm_seq = req.body.cfm_seq;
-	var cfm_yn = req.body.cfm_yn;
-	var cfm_opinion = req.body.cfm_opinion;
-	connection.query(
-		'update confirm set cfm_yn=?,cfm_opinion=? where en=? and task_id=? and cfm_seq=?',
-		[ en, task_id, cfm_seq, cfm_yn, cfm_opinion ],
-		function(err,result) {
-			if (err) {
-				res.send(JSON.stringify(err));
-			} else {
-				res.send(JSON.stringify(result));
-			}
-		}
-	);
+		});
 });
 
 router.put('/confirms', function(req, res) {
 	var confirmData = req.body.confirmData;
-	var data = JSON.parse(confirmData);
-	if (data.hasOwnProperty("Reference")) {
-      for(var i = 0; i < data.Reference.length; i++; ) {
-        try {
-            var reference = data.Reference[i];
-            connection.query(
-				'update confirm set cfm_yn="Y",cfm_opinion="결재" where en=? and task_id=? and cfm_seq=?',
-				[ reference.en, reference.task_id, reference.cfm_seq ],
-				function(err,result) {
-					if (err) {
-						res.send(JSON.stringify(err));
-					} else {
-						res.send(JSON.stringify(result));
-					}
-				}
-			);
-        } catch(e) {
-            //In case if any invalid references, then continue with others
-            console.warn("Error processing reference - " + e);
-        }
-      }
-    } else {
-      console.warn("No References were found!");
-    }
-	
-});
-
-router.put('/reject', function(req, res) {
-	var en = req.body.en;
-	var task_id = req.body.task_id;
-	var cfm_seq = req.body.cfm_seq;
-	var cfm_yn = req.body.cfm_yn;
-	var cfm_opinion = req.body.cfm_opinion;
-	connection.query(
-		'update confirm set cfm_yn=?,cfm_opinion=? where en=? and task_id=? and cfm_seq=?',
-		[ en, task_id, cfm_seq, cfm_yn, cfm_opinion ],
-		function(err,result) {
-			if (err) {
-				res.send(JSON.stringify(err));
-			} else {
-				res.send(JSON.stringify(result));
-			}
-		}
-	);
-});
-
-router.get('/view', function(req, res) {
-	var en = req.query.en;
-	var task_id = req.query.task_id;
-	var cfm_seq = req.query.cfm_seq;
-	connection.query(
-		'select en,task_id,cfm_seq,cfm_text,cfm_yn,cfm_opinion from confirm where en=? and task_id=? and cfm_seq=?',
-		[ en, task_id, cfm_seq ],
-		function(err,results,fields) {
-			if (err) {
-				res.send(JSON.stringify(err));
-			} else {
-				if (results.length > 0) {
-					res.send(JSON.stringify(results));	
+	    confirmData = JSON.parse(confirmData);
+	var results = [];
+	for(var i = 0; i < confirmData.length; i++ ) {
+        var data = confirmData[i];
+        connection.query(
+			'update confirm set cfm_yn="Y",cfm_opinion="결재" where en=? and task_id=? and cfm_seq=?',
+			[ data.en, data.task_id, data.cfm_seq ],
+			function(err,result) {
+				if (err) {
+					res.send(JSON.stringify(err));
 				} else {
-					res.send(JSON.stringify({}));
-				}				
-			}
-		}
-	);
+					results[i] = result;
+					//res.send(JSON.stringify(result));
+				}
+			});
+    }
+    res.send(JSON.stringify(results));
+});
+
+router.put('/rejects', function(req, res) {
+	var rejectData = req.body.rejectData;
+	    rejectData = JSON.parse(rejectData);
+	var results = [];
+	for(var i = 0; i < rejectData.length; i++ ) {
+        var data = rejectData[i];
+        connection.query(
+			'update confirm set cfm_yn="N",cfm_opinion="반려" where en=? and task_id=? and cfm_seq=?',
+			[ data.en, data.task_id, data.cfm_seq ],
+			function(err,result) {
+				if (err) {
+					res.send(JSON.stringify(err));
+				} else {
+					res.send(JSON.stringify(result));
+				}
+			});
+    }
+    res.send(JSON.stringify(results));
 });
 
 router.get('/pushset', function(req, res) {
@@ -187,71 +280,55 @@ router.get('/pushset', function(req, res) {
 				res.send(JSON.stringify(err));
 			} else {
 				if (results.length > 0) {
-					res.send(JSON.stringify(results[0]));	
+					res.send(JSON.stringify(results));
 				} else {
 					res.send(JSON.stringify({}));
-				}				
+				}
 			}
-		}
-	);
-});
-
-router.post('/pushset', function(req, res) {
-	var en = req.body.en;
-	var pushsets = req.body.pushsets;
-		pushsets = JSON.parse(pushsets);
-	for (var i = 0; i < pushsets.length; i++) {
-		var pushset = pushsets[i]
-		pushset.id
-		pushset.yn
-	}	
-	res.send(JSON.stringify({en:en,pushsets:pushsets}));
+		});
 });
 
 router.put('/pushset', function(req, res) {
 	var en = req.body.en;
-	var pushsets = req.body.pushsets;
-		pushsets = JSON.parse(pushsets);
-	for (var i = 0; i < pushsets.length; i++) {
-		var pushset = pushsets[i]
-		pushset.id
-		pushset.yn
-	}	
-	res.send(JSON.stringify({en:en,pushsets:pushsets}));
+	var results = [];
+	var pushsetData = req.body.pushsetData;
+		pushsetData = JSON.parse(pushsetData);
+	for (var i = 0; i < pushsetData.length; i++) {
+		var data = pushsetData[i]
+		connection.query(
+			'select a.en, a.task_id, a.push_yn, b.task_name from pushsets a right outer join task b on a.task_id = b.task_id where a.en = ? and a.task_id = ?',
+			[ req.body.en, data.task_id ],
+			function(err,results,fields) {
+				if (err) {
+					res.send(JSON.stringify(err));
+				} else {
+					if (results.length > 0) {
+						connection.query(
+							'update pushsets set push_yn=? where en=? and task_id=?',
+							[ data.push_yn, req.body.en, data.task_id ],
+							function(err,result) {
+								if (err) {
+									res.send(JSON.stringify(err));
+								} else {
+									results[i] = result;
+								}
+							});
+					} else {
+						connection.query(
+							'insert into pushsets(en,task_id,push_yn) values(?,?,?)',
+							[ req.body.en, data.task_id, data.push_yn ],
+							function(err,result) {
+								if (err) {
+									res.send(JSON.stringify(err));
+								} else {
+									results[i] = result;
+								}
+							});
+					}
+				}
+			});
+	}
+	res.send(JSON.stringify(results));
 });
 
-/*
-router.post('/user/login', function(req, res) {
-	var id = req.body.id;
-	var password = req.body.password;
-	res.send(JSON.stringify({id:id,password:password}));
-});
-
-router.post('/user', function(req, res) {
-	var id = req.body.id;
-	var password = req.body.password;
-	res.send(JSON.stringify({id:id,password:password}));
-});
-
-router.get('/user', function(req, res) {
-	var rowid = req.query.rowid;
-	res.send(JSON.stringify({rowid:rowid}));
-});
-
-router.put('/user', function(req, res) {
-	var rowid = req.body.rowid;
-	var id = req.body.id;
-	var password = req.body.password;
-	res.send(JSON.stringify({rowid:rowid,id:id,password:password}));
-});
-
-router.delete('/user', function(req, res) {
-	var rowid = req.body.rowid;
-	res.send(JSON.stringify({rowid:rowid}));
-});
-
-router.get('/user/list', function(req, res) {
-	res.send(JSON.stringify([]));
-});
-*/
 module.exports = router;
